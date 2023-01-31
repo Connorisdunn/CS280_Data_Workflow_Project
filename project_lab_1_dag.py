4514338eaec396ed2569c9855366801eb182e804
Connorisdunn <97138356+Connorisdunn@users.noreply.github.com>
2023-01-30 18:58:24 -0700

    Update project_lab_1_dag.py

--- project_lab_1_dag.py
+++ project_lab_1_dag.py
@@ -11,28 +11,15 @@ import pendulum
 #print(my_value)
 #Variable.get(f"my_list_key", [], deserialize_json=True)
 
-my_task = PythonOperator(
-        task_id="my_dummy_task",
-        python_callable=my_task_func,
-        provide_context=True,
-    )
-my_task_two = PythonOperator(
-    task_id="my_dummy_task_2",
-    python_callable=my_task_func_2,
-    provide_context=True,
-)
+def my_task_func(ti: TaskInstance, **kwargs):
+  my_list = [1,2,3,4,5]
+  ti.xcom_push("i_love_ds", my_list)
 
 
-
-get_twitter_api_data_task = PythonOperator(
-    task_id="twitter_api_task",
-    python_callable=get_twitter_api_data_task_func,
-    provide_context=True
-)
-
-def get_auth_header():
-    my_bearer_token = Variable.get("TWITTER_BEARER_TOKEN", deserialize_json=True)
-    return {"Authorization": f"Bearer {my_bearer_token}"}
+def my_task_func_2(ti: TaskInstance, **kwargs):
+  my_list = ti.xcom_pull(key="i_love_ds", task_ids="my_dummy_task")
+  logging.info(my_list)
+  #Should log the list [1,2,3,4,5] to this task's log.
 
 def get_twitter_api(ti: TaskInstance, **kwargs):
     user_ids = Variable.get("TWITTER_USER_IDS", deserialize_json=True)
@@ -44,31 +31,32 @@ def get_twitter_api(ti: TaskInstance, **kwargs):
     logging.info(user_requests,tweet_requests)
 
 
+def get_auth_header():
+    my_bearer_token = Variable.get("TWITTER_BEARER_TOKEN", deserialize_json=True)
+    return {"Authorization": f"Bearer {my_bearer_token}"}
 
-#def get_auth_header():
-  #my_bearer_token = "AAAAAAAAAAAAAAAAAAAAAGrdlQEAAAAAzVmWY9pqSyvsR0QLUVisknVWiRU%3DHebGz3s9dv8b2uo3oOiaBol3HE0287o3DPpqml0G7BCw9xcVY6"
-  #return {"Authorization": f"Bearer {my_bearer_token}"}
-
-
-
-def my_task_func(ti: TaskInstance, **kwargs):
-  my_list = [1,2,3,4,5]
-  ti.xcom_push("i_love_ds", my_list)
-
-def my_task_func_2(ti: TaskInstance, **kwargs):
-  my_list = ti.xcom_pull(key="i_love_ds", task_ids="my_dummy_task")
-  logging.info(my_list)
-  #Should log the list [1,2,3,4,5] to this task's log.
 
 with DAG(
     dag_id="project_lab_1_etl",
     schedule_interval="0 9 * * *",
     start_date=pendulum.datetime(2023, 1, 1, tz="US/Pacific"),
     catchup=False,
-) as gad: 
-    my_dummy_task
-    my_dummy_task_2
-    twitter_api_task
+) as dag: 
+    my_dummy_task = PythonOperator(
+        task_id="my_dummy_task",
+        python_callable= my_task_func,
+        provide_context=True,
+    )
+    my_dummy_task_2 = PythonOperator(
+        task_id="my_dummy_task_2",
+        python_callable= my_task_func_2,
+        provide_context=True,
+    )
+    twitter_api_task = PythonOperator(
+        task_id="twitter_api_task",
+        python_callable=get_twitter_api,
+        provide_context=True
+    )
 
 
 my_dummy_task >> my_dummy_task_2 >> twitter_api_task
