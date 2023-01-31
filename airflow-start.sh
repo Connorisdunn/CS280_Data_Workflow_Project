9b83cc14224ac2728a76c9798fff3477425127b9
Connorisdunn <97138356+Connorisdunn@users.noreply.github.com>
2023-01-30 19:53:30 -0700

    Update airflow-start.sh

--- airflow-start.sh
+++ airflow-start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export AIRFLOW_HOME=/home/<Your User Here>/airflow-cs280
-cd /home/<Your User Here>/airflow-cs280
-source /home/<Your User Here>/miniconda3/bin/activate
-conda activate /home/<Your User Here>/miniconda3/envs/airflow-env
+export AIRFLOW_HOME=/home/connorisdunn/airflow-cs280
+export GOOGLE_APPLICATION_CREDENTIALS="/home/connorisdunn/airflow-cs280/auth/bucket_auth.json"
+cd /home/connorisdunn/airflow-cs280
+source /home/connorisdunn/miniconda3/bin/activate
+conda activate /home/connorisdunn/miniconda3/envs/airflow-env
 nohup airflow scheduler >> scheduler.log &
 nohup airflow webserver -p 8080 >> webserver.log &
