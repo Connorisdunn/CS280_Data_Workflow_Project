1e7249fcf6235446b540def0ac6cb1fc695069da
Connorisdunn <97138356+Connorisdunn@users.noreply.github.com>
2023-01-30 18:45:01 -0700

    Update project_lab_1_dag.py

--- project_lab_1_dag.py
+++ project_lab_1_dag.py
@@ -31,14 +31,14 @@ def get_auth_header():
     my_bearer_token = Variable.get("TWITTER_BEARER_TOKEN", deserialize_json=True)
     return {"Authorization": f"Bearer {my_bearer_token}"}
 
-def get_twitter_api():
+def get_twitter_api(ti: TaskInstance, **kwargs):
     user_ids = Variable.get("TWITTER_USER_IDS", deserialize_json=True)
     tweet_ids = Variable.get("TWITTER_TWEET_IDS", deserialize_json=True)
     user_requests = [requests.get(f"https://api.twitter.com/2/users/{id}?user.fields=public_metrics,profile_image_url,username,id,description", headers=get_auth_header()) for id in user_ids]
     tweet_requests = [requests.get(f"https://api.twitter.com/2/tweets/{id}?tweet.fields=author_id,text,public_metrics", headers=get_auth_header()) for id in tweet_ids]
     ti.xcom_push("user_requests", user_requests)
     ti.xcom_push("tweet_requests", tweet_requests)
-    logging.info(user_requests, tweet_requests)
+    logging.info(user_requests,tweet_requests)
 
 
 
