282702f6159fa65d536e6c07e9f991842b5c780d
Connorisdunn <97138356+Connorisdunn@users.noreply.github.com>
2023-01-30 19:05:29 -0700

    Update project_lab_1_dag.py

--- project_lab_1_dag.py
+++ project_lab_1_dag.py
@@ -1,25 +1,16 @@
-import requests
-from airflow.models import Variable
-from airflow.models import TaskInstance
-from airflow.operators.python import PythonOperator
 from airflow import DAG
 import logging
 import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.models import Variable
+from airflow.models import TaskInstance
+import requests
+import pandas as pd
 
-
-#my_value = Variable.get("my_key", deserialize_json=True)
-#print(my_value)
-#Variable.get(f"my_list_key", [], deserialize_json=True)
-
-def my_task_func(ti: TaskInstance, **kwargs):
-  my_list = [1,2,3,4,5]
-  ti.xcom_push("i_love_ds", my_list)
-
-
-def my_task_func_2(ti: TaskInstance, **kwargs):
-  my_list = ti.xcom_pull(key="i_love_ds", task_ids="my_dummy_task")
-  logging.info(my_list)
-  #Should log the list [1,2,3,4,5] to this task's log.
+def get_auth_header():
+    my_bearer_token = Variable.get("TWITTER_BEARER_TOKEN", deserialize_json=True)
+    return {"Authorization": f"Bearer {my_bearer_token}"}
 
 def get_twitter_api(ti: TaskInstance, **kwargs):
     user_ids = Variable.get("TWITTER_USER_IDS", deserialize_json=True)
@@ -30,10 +21,9 @@ def get_twitter_api(ti: TaskInstance, **kwargs):
     ti.xcom_push("tweet_requests", tweet_requests)
     logging.info(user_requests,tweet_requests)
 
-
-def get_auth_header():
-    my_bearer_token = Variable.get("TWITTER_BEARER_TOKEN", deserialize_json=True)
-    return {"Authorization": f"Bearer {my_bearer_token}"}
+def transform_twitter_api_data_func(ti: TaskInstance, **kwargs):
+    user_requests = pd.DataFrame(data=ti.xcom_pull(key="user_requests", task_ids="get_twitter_api_data_task"))
+    tweet_requests = pd.DataFrame(data=ti.xcom_pull(key="tweet_requests", task_ids="get_twitter_api_data_task"))
 
 
 with DAG(
@@ -41,28 +31,16 @@ with DAG(
     schedule_interval="0 9 * * *",
     start_date=pendulum.datetime(2023, 1, 1, tz="US/Pacific"),
     catchup=False,
-) as dag: 
-    my_dummy_task = PythonOperator(
-        task_id="my_dummy_task",
-        python_callable= my_task_func,
-        provide_context=True,
-    )
-    my_dummy_task_2 = PythonOperator(
-        task_id="my_dummy_task_2",
-        python_callable= my_task_func_2,
-        provide_context=True,
-    )
-    twitter_api_task = PythonOperator(
-        task_id="twitter_api_task",
+) as dag:
+    get_twitter_api_data_task = PythonOperator(
+        task_id="get_twitter_api_data_task", 
         python_callable=get_twitter_api,
         provide_context=True
     )
+    transform_twitter_api_data_task = PythonOperator(
+        task_id="transform_twitter_api_data_task", 
+        python_callable=transform_twitter_api_data_func,
+        provide_context=True
+    )
 
-
-my_dummy_task >> my_dummy_task_2 >> twitter_api_task
-
-
-#user_id = "44196397"
-#api_url = f"https://api.twitter.com/2/users/{user_id}"
-#request = requests.get(api_url, headers=get_auth_header())
-#print(request.json())
+get_twitter_api_data_task >> transform_twitter_api_data_task
