73c4157df7acc4316310be9cecbdf3983c79c74f
Connorisdunn <97138356+Connorisdunn@users.noreply.github.com>
2023-02-01 22:58:04 -0700

    Update project_lab_1_dag.py

--- project_lab_1_dag.py
+++ project_lab_1_dag.py
@@ -1,42 +1,94 @@
 from airflow import DAG
-from google.cloud import storage
 import logging
 import pendulum
 from airflow.operators.python import PythonOperator
-from airflow.operators.dummy import DummyOperator
 from airflow.models import Variable
 from airflow.models import TaskInstance
 import requests
 import pandas as pd
+from google.cloud import storage
+from gcsfs import GCSFileSystem
+import json
+from databox import Client
+import csv
 
-def get_auth_header():
-    my_bearer_token = Variable.get("TWITTER_BEARER_TOKEN", deserialize_json=True)
-    return {"Authorization": f"Bearer {my_bearer_token}"}
+def flatten_json(data_dict, matching_data, keys_to_match):
+    for key, value in data_dict.items():
+        if type(value) is dict:
+            flatten_json(value, matching_data, keys_to_match)
+        else:
+            if key in keys_to_match:
+                matching_data.append(f"{key}: {value}")
+    return matching_data
+
+def iterate_json_list(data_dict, keys_to_match):
+    match_list = []
+    for dicti in data_dict:
+        matches = []
+        if type(dicti) is dict:
+            flatten_json(dicti, matches, keys_to_match)
+        matches = {x.split(": ")[0]: x.split(": ")[1] for x in matches}
+        matches = dict(sorted(matches.items()))
+        match_list.append(matches)
+    return pd.DataFrame(match_list)
 
 def get_twitter_api(ti: TaskInstance, **kwargs):
     user_ids = Variable.get("TWITTER_USER_IDS", deserialize_json=True)
     tweet_ids = Variable.get("TWITTER_TWEET_IDS", deserialize_json=True)
-    my_bearer_token = "AAAAAAAAAAAAAAAAAAAAAGrdlQEAAAAAzVmWY9pqSyvsR0QLUVisknVWiRU%3DHebGz3s9dv8b2uo3oOiaBol3HE0287o3DPpqml0G7BCw9xcVY6"
+    my_bearer_token = Variable.get("TWITTER_BEARER_TOKEN")
     header_token = {"Authorization": f"Bearer {my_bearer_token}"}
     user_requests = [requests.get(f"https://api.twitter.com/2/users/{id}?user.fields=public_metrics,profile_image_url,username,id,description", headers=header_token).json() for id in user_ids]
     tweet_requests = [requests.get(f"https://api.twitter.com/2/tweets/{id}?tweet.fields=author_id,text,public_metrics", headers=header_token).json() for id in tweet_ids]
-    ti.xcom_push("user_requests", user_requests)
-    ti.xcom_push("tweet_requests", tweet_requests)
+    ti.xcom_push("user_requests", json.dumps(user_requests))
+    ti.xcom_push("tweet_requests", json.dumps(tweet_requests))
     logging.info(user_requests)
     logging.info(tweet_requests)
 
 def transform_twitter_api_data_func(ti: TaskInstance, **kwargs):
-    user_requests = pd.DataFrame(data=ti.xcom_pull(key="user_requests", task_ids="get_twitter_api_data_task"))
-    tweet_requests = pd.DataFrame(data=ti.xcom_pull(key="tweet_requests", task_ids="get_twitter_api_data_task"))
+    users = data=ti.xcom_pull(key="user_requests", task_ids="get_twitter_api_data_task")
+    tweets = data=ti.xcom_pull(key="tweet_requests", task_ids="get_twitter_api_data_task")
+    tweet_header_list = ['retweet_count', 'reply_count', 'like_count', 'quote_count', 'impression_count', 'tweet_id', 'text', 'id']
+    user_header_list = ['followers_count','following_count','tweet_count','listed_count','name','username','id']
+    user_matching_data = iterate_json_list(json.loads(users), user_header_list)
+    tweet_matching_data = iterate_json_list(json.loads(tweets), tweet_header_list)
+    
     client = storage.Client()
     bucket = client.get_bucket("c-d-apache-airflow-cs280")
-    bucket.blob("data/tweet.csv").upload_from_string(tweet_requests.to_csv(index=False), "text/csv")
-    bucket.blob("data/user.csv").upload_from_string(user_requests.to_csv(index=False), "text/csv")
+    bucket.blob("data/user_requests.csv").upload_from_string(user_matching_data.to_csv(index=False), "text/csv")
+    bucket.blob("data/tweet_requests.csv").upload_from_string(tweet_matching_data.to_csv(index=False), "text/csv")
+
+    user_token = Variable.get("DATABOX_TOKEN")
+    dbox = Client(user_token)
+    
+    fs = GCSFileSystem(project="Connor-Dunn-CS-280")
+    with fs.open('gs://c-d-apache-airflow-cs280/data/user_requests.csv', 'r') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        data = [row for row in reader]
+        test_list = ['followers_count','following_count','listed_count','tweet_count']
+        for val in data:
+            for idx, item in enumerate(val):
+                count = test_list.count(header[idx])
+                name_index = header.index('name')
+                if count > 0:
+                    dbox.push(f'UserMetric {val[name_index]} {header[idx]}', item)
+    with fs.open('gs://c-d-apache-airflow-cs280/data/tweet_requests.csv', 'r') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        data = [row for row in reader]
+        test_list = ['reply_count','like_count','impression_count','retweet_count']
+        for val in data:
+            for idx, item in enumerate(val):
+                count = test_list.count(header[idx])
+                tweet_index = header.index('id')
+                if count > 0:
+                    dbox.push(f'TweetMetric {val[tweet_index]} {header[idx]}', item)
+
 
 
 with DAG(
     dag_id="project_lab_1_etl",
-    schedule_interval="0 9 * * *",
+    schedule_interval="0 9 * * *", # schedule_interval="*/1 * * * *",
     start_date=pendulum.datetime(2023, 1, 1, tz="US/Pacific"),
     catchup=False,
 ) as dag:
